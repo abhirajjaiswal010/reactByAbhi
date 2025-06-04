6317e6409314fa2b7e3fe8840d85536443bbf9ec
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2025-06-04 19:17:46 +0530

    initial-commit

---
+++ todoList/src/components/todoList.jsx
@@ -0,0 +1,23 @@
+import { LuBadgeCheck } from "react-icons/lu";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+export const TodoList = ({data,onHandleDeleteTodo,lastAddedList,isClearingList,checked,onHandleCheckedTodo}) => {
+  return (
+    <li
+      
+      className={`${data === lastAddedList ? "task-enter" : ""} ${
+        isClearingList ? "fade-out" : ""
+      }`}>
+      <span className={checked?"checkList":"notCheckList"}>{data}</span>
+      {/* task done btn */}
+      <button className="check_btn" onClick={() => onHandleCheckedTodo(data)}>
+        <LuBadgeCheck />
+      </button>
+      {/* task delete btn */}
+      <button
+        className="Delete_btn"
+        onClick={() => onHandleDeleteTodo(data)}>
+        <MdOutlineDeleteOutline />
+      </button>
+    </li>
+  );
+};
